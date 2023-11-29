875ae13e930bdd9bd220299d185ae2f9f0b9f832
Hudz Oleksandr <zefffs@gmai.com>
2023-11-29 22:02:14 +0200

    test

--- src/main/java/ua/hellil/hudz/lessons/lesson3/Main.java
+++ src/main/java/ua/hellil/hudz/lessons/lesson3/Main.java
@@ -3,6 +3,10 @@ package ua.hellil.hudz.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+
 
     }
 }
