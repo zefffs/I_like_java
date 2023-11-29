51690295429291196fd1e9b4b434468953813c54
zefffs <152438671+zefffs@users.noreply.github.com>
2023-11-29 22:12:45 +0200

    from GitHUB

--- src/main/java/ua/hellil/hudz/lessons/lesson3/Main.java
+++ src/main/java/ua/hellil/hudz/lessons/lesson3/Main.java
@@ -3,9 +3,7 @@ package ua.hellil.hudz.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
+        System.out.println("Hello JAVA");
 
 
     }
