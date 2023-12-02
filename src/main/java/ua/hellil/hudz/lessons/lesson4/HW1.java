6227dc40e2fb376f6f8372928b91ac1a811e51a1
Hudz Oleksandr <zefffs@gmai.com>
2023-12-02 05:15:58 +0200

    Домашня робота до заняття 4.

--- src/main/java/ua/hellil/hudz/lessons/lesson4/HW1.java
+++ src/main/java/ua/hellil/hudz/lessons/lesson4/HW1.java
@@ -3,6 +3,41 @@ package ua.hellil.hudz.lessons.lesson4;
 public class HW1 {
     public static void main(String[] args) {
 
+//        Паралелепіпед:
+//
+//        Створити у новому класі main-метод;
+//
+//        Створити три змінні для зберігання сторін паралелепіпеда;
+//
+//        Порахувати обсяг у змінній volume;
+//
+//        Вивести на екран змінну volume у вигляді:
+//
+//        System.out.println("Об'єм паралелепіпеда = " + volume);
+//
+//        Порахувати сумарну довжину всіх сторін у змінній length та вивести її на екран;
+//
+//        Прикріпити до LMS файл .java з вашою роботою;
+//
+//        Файл .java лежить за таким шляхом:
+//
+//        папка проекту -> src -> your_package_name -> MyApp.java
+//
+//        де MyApp - назва вашого класу/файлу
+
+
+        double volume;
+        double length;
+        double a = 12;
+        double b = 10;
+        double c = 8;
+
+        volume = a * b * c;
+        length = (a + b + c) * 4;
+
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        System.out.println("Сумарна довжина всіх сторін = " + length);
+
 
     }
 }
--- src/main/java/ua/hellil/hudz/lessons/lesson4/HW2.java
+++ src/main/java/ua/hellil/hudz/lessons/lesson4/HW2.java
@@ -3,6 +3,46 @@ package ua.hellil.hudz.lessons.lesson4;
 public class HW2 {
     public static void main(String[] args) {
 
+//        За владу в стародавньому Китаї боролися дві династії: Лі та Мінь.
+//
+//                З кожного боку боролися 3 типи воїнів з такими показниками атаки:
+//
+//        Лі:
+//
+//        воїн - 13
+//        лучник - 24
+//        вершник - 46
+//
+//        Мінь:
+//
+//        воїн - 9
+//        лучник - 35
+//        вершник - 12
+//        Розрахувати: загальний показник атаки для обох династій, якщо відомо, що
+//        воїнів династії Лі кожного типу було 860, а кількість воїнів династії Мінь
+//        кожного типу в півтора рази більша.
+//
+//        Вивести в консоль значення загальної атаки кожної армії;
+
+
+         int liWarion = 13;
+         int liArcher = 24;
+         int liRider = 46;
+         int liNuberOfPeople = 860;
+
+         int minWarion = 9;
+         int minArcher = 35;
+         int minRider = 12;
+         int minNuberOfPeople = (int) (liNuberOfPeople * 1.5);
+
+         int liStrong = (liArcher + liRider + liWarion) * liNuberOfPeople;
+         int minStrong = (minArcher + minRider + minWarion) * minNuberOfPeople;
+
+         System.out.println("Загальний показник атаки династії Лі = " + liStrong);
+         System.out.println("Загальний показник атаки династії Мінь = " + minStrong);
+
+
+
 
     }
 }
