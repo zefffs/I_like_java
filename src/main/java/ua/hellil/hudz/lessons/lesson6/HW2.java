329be4de771b6720ef947fa22d91d5d71cbe22a1
Hudz Oleksandr <zefffs@gmai.com>
2023-12-16 18:57:07 +0200

    Домашня робота Counter Strike.

---
+++ src/main/java/ua/hellil/hudz/lessons/lesson6/HW2.java
@@ -0,0 +1,51 @@
+package ua.hellil.hudz.lessons.lesson6;
+
+import java.util.Scanner;
+
+public class HW2 {
+    public static void main(String[] args) {
+
+//        Зробити гру, в якій потрібно відгадати ціле число, яке "загадав" комп'ютер у
+//        діапазоні від 0 до 10
+//        Користувач повинен мати можливість вгадувати в кілька спроб (мінімум 3).
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numPlayer = 0;
+        int pop = 3;
+        int numPC = (int) Math.round(Math.random() * 10);
+
+        System.out.println("Відгадай ціле число, яке я загадав в діапазоні від 0 до 10.");
+
+
+
+//        System.out.println(nuberPC);
+
+        while (true){
+            System.out.println( "У тебe " + pop + " спроби!");
+            if (scanner.hasNextInt()){
+                 numPlayer = scanner.nextInt();
+                 scanner.nextLine();
+
+
+            } else {
+                System.out.println("Wrong date!!!!");
+                scanner.nextLine();
+            }
+
+            if (numPC == numPlayer) {
+                System.out.println("Ти виграв. Молодець!!!");
+                break;
+            }
+
+            pop--;
+
+            if (pop == 0){
+                System.out.println("Ти програв число яке я загадав  " + numPC);
+                break;
+            }
+
+
+        }
+    }
+}
