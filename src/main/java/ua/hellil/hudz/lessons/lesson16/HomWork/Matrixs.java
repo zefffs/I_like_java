9cc6bc51700615c4ed3900bad4d9709424fea7ce
Hudz Oleksandr <zefffs@gmai.com>
2024-02-04 19:21:00 +0200

    Домашка до заняття 16 версія 1

---
+++ src/main/java/ua/hellil/hudz/lessons/lesson16/HomWork/Matrixs.java
@@ -0,0 +1,81 @@
+package ua.hellil.hudz.lessons.lesson16.HomWork;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Matrixs {
+    public static void main(String[] args) {
+
+//        Додайте метод, який буде рахувати середнє арифметичне елементів масиву.
+//        Зробіть метод, який прийматиме на вхід двомірну матрицю (двовимірний масив) у та перевіряти чи є вона квадратною
+//        В тестовому класі створіть тестову матрицю та передайте її в метод тестованого класу
+
+
+//        if array == null ----> -1
+//        if array empty -----> -2
+//        if array is Square ------> true
+//        else is not square, empty, null ------> false
+//
+
+        int [][] array = {
+                {},
+                {},
+                {1,3,4}
+
+        };
+
+//        int [][] array = new int[1][1];
+        System.out.println(Arrays.toString(array[0]));
+//
+//        int [][] array = new int[0][0];
+
+
+        System.out.println(averageAremetic(array));
+        System.out.println(isSquare(array));
+
+
+    }
+   public static double averageAremetic (int[][] array ) {
+
+
+       if (array == null){
+           return -1;
+       }
+
+//       if (array.length == 0){
+//           return -2;
+//       }
+
+       int sumMat = 0;
+       int count = 0;
+
+       for (int i = 0; i < array.length; i++) {
+           for (int j = 0; j < array[i].length; j++) {
+               sumMat += array[i][j];
+               count++;
+           }
+       }
+
+       if (sumMat == 0) {
+           return -2;
+       }
+
+       return (double) sumMat / count;
+   }
+
+   public static boolean isSquare(int[][] array ) {
+
+        if (array == null || array.length == 0){
+            return false;
+        }
+
+       for (int[] ints : array) {
+
+           if (ints.length != array.length){
+               return false;
+           }
+       }
+       return true;
+   }
+
+}
---
+++ src/test/java/ua/hellil/hudz/lesson16/HomeWork/TestMatrixs.java
@@ -0,0 +1,51 @@
+package ua.hellil.hudz.lesson16.HomeWork;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ua.hellil.hudz.lessons.lesson16.HomWork.Matrixs;
+
+public class TestMatrixs {
+
+    @Test
+    void testNull (){
+        int [][] array = null;
+        Assertions.assertEquals(-1, Matrixs.averageAremetic(array));
+    }
+
+    @Test
+    void testEmpty(){
+        int [][] array = new int[0][0];
+        Assertions.assertEquals(-2, Matrixs.averageAremetic(array));
+    }
+
+    @Test
+    void testIsSquare(){
+        int [][] array = new int[2][2];
+        Assertions.assertTrue(true == Matrixs.isSquare(array));
+    }
+
+    @Test
+    void testIsNotSquare(){
+        int [][] array = new int[1][2];
+        Assertions.assertTrue(false == Matrixs.isSquare(array));
+    }
+
+    @Test
+    void testIsEmptyMatrix(){
+//        int [][] array = new int[0][0];
+        int [][] array = {
+
+        };
+        Assertions.assertTrue(false == Matrixs.isSquare(array));
+    }
+
+    @Test
+    void testIsNullMatrix(){
+        int [][] array = null;
+        Assertions.assertTrue(false == Matrixs.isSquare(array));
+    }
+
+
+
+
+}
